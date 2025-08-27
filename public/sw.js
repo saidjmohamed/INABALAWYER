b5b1d5ae4634aff92cd6e1ec5d57c484d69c8f3c
saidjmohamed <64891321+saidjmohamed@users.noreply.github.com>
2025-08-27 23:31:29 +0100

    [dyad] تحويل التطبيق إلى PWA - wrote 3 file(s)

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// This is the service worker file.
+
+self.addEventListener('install', event => {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
