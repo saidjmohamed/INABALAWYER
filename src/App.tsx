2c81329f3984904435d91a5c135d19fed390f4d5
اsaidj mohamed <saidj.mohamed@gmail.com>
2025-09-12 14:54:48 +0100

    [dyad] Fixed SettingsProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppWrapper } from './components/AppWrapper';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
+import { SettingsProvider } from './contexts/SettingsContext';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
@@ -23,27 +24,29 @@ import MaintenancePage from './pages/MaintenancePage';
 function App() {
   return (
     <BrowserRouter>
-      <AppWrapper>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-          <Route path="/lawyers/:id" element={<LawyerProfilePage />} />
-          <Route path="/cases" element={<ProtectedRoute><CasesPage /></ProtectedRoute>} />
-          <Route path="/cases/new" element={<ProtectedRoute><CreateCasePage /></ProtectedRoute>} />
-          <Route path="/cases/:id" element={<ProtectedRoute><CaseDetailsPage /></ProtectedRoute>} />
-          <Route path="/cases/:id/edit" element={<ProtectedRoute><AdminEditCasePage /></ProtectedRoute>} />
-          <Route path="/conversations" element={<ProtectedRoute><ConversationsPage /></ProtectedRoute>} />
-          <Route path="/conversations/:id" element={<ProtectedRoute><ConversationsPage /></ProtectedRoute>} />
-          <Route path="/lawyers" element={<ProtectedRoute><LawyersDirectory /></ProtectedRoute>} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/courts" element={<CourtsListPage />} />
-          <Route path="/courts/:id" element={<CourtDetailsPage />} />
-          <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </AppWrapper>
+      <SettingsProvider>
+        <AppWrapper>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+            <Route path="/lawyers/:id" element={<LawyerProfilePage />} />
+            <Route path="/cases" element={<ProtectedRoute><CasesPage /></ProtectedRoute>} />
+            <Route path="/cases/new" element={<ProtectedRoute><CreateCasePage /></ProtectedRoute>} />
+            <Route path="/cases/:id" element={<ProtectedRoute><CaseDetailsPage /></ProtectedRoute>} />
+            <Route path="/cases/:id/edit" element={<ProtectedRoute><AdminEditCasePage /></ProtectedRoute>} />
+            <Route path="/conversations" element={<ProtectedRoute><ConversationsPage /></ProtectedRoute>} />
+            <Route path="/conversations/:id" element={<ProtectedRoute><ConversationsPage /></ProtectedRoute>} />
+            <Route path="/lawyers" element={<ProtectedRoute><LawyersDirectory /></ProtectedRoute>} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/courts" element={<CourtsListPage />} />
+            <Route path="/courts/:id" element={<CourtDetailsPage />} />
+            <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppWrapper>
+      </SettingsProvider>
     </BrowserRouter>
   );
 }
