c9855445244e5bdbb423ad4e0c82b731c23584b7
اsaidj mohamed <saidj.mohamed@gmail.com>
2025-09-12 14:52:50 +0100

    [dyad] Fixed AppWrapper export - wrote 1 file(s)

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
 import { useSettings } from '../contexts/SettingsContext';
 import { useSession } from '../contexts/SessionContext';
-// ... rest of the code
+import MaintenancePage from '../pages/MaintenancePage';
+
+export const AppWrapper = ({ children }: { children: React.ReactNode }) => {
+  const { isMaintenanceMode } = useSettings();
+  const { session } = useSession();
+
+  if (isMaintenanceMode && !session) {
+    return <MaintenancePage />;
+  }
+
+  return <>{children}</>;
+};
